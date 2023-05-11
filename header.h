f83647a3e8ed8d42cc39f29d7c3342f96cafd389
Carolynee1 <c.ajisegiri9@gmail.com>
2023-05-11 08:52:43 -0700

    header.h

---
+++ header.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 80
+
+#endif
