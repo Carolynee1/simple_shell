24af3c07e8103143d848b310f60dde5822e23300
Carolynee1 <c.ajisegiri9@gmail.com>
2023-05-26 14:15:53 -0700

    simple shell update

---
+++ input_san.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+int status;
+
+int line_num;
+
+char *shell_name;
+
+/**
+ * sanitize_input - sanitizes input from the command line
+ * @old_buf: buffer to be sanitized
+ * @old_size: size of old buffer
+ *
+ * Return: the new, sanitized buffer
+ */
+char *sanitize_input(char *old_buf, size_t *old_size)
+{
+	char *new_buf = malloc(*old_size * 3);
+	char *new_ptr = new_buf;
+	char *old_ptr = old_buf;
+
+	while (*old_ptr != '\0')
+	{
+		while (*old_ptr == ' ')
+			old_ptr++;
+		while (*old_ptr	!= ' ' && *old_ptr != ';' && *old_ptr != '|'
+		       && *old_ptr != '&' && *old_ptr != '\0')
+		{
+			*new_ptr = *old_ptr;
+			new_ptr++;
+			old_ptr++;
+		}
+		while (*old_ptr == ' ')
+			old_ptr++;
+		if (new_ptr != new_buf && *old_ptr != '\0')
+		{
+			*new_ptr = ' ';
+			new_ptr++;
+		}
+
+		if (*old_ptr == ';' || *old_ptr == '|' || *old_ptr == '&')
+		{
+			if (check_input_error(old_ptr) == FALSE)
+			{
+				*old_size = 0;
+				break;
+			}
+			*new_ptr = *old_ptr;
+			if (*old_ptr == ';')
+			{
+				new_ptr++;
+				*new_ptr = ' ';
+			}
+			else if (*(old_ptr + 1) == *old_ptr)
+			{
+				if (new_ptr == new_buf)
+				{
+					print_error_msg(old_ptr, NULL);
+					*old_size = 0;
+					break;
+				}
+				new_ptr++;
+				*new_ptr = *old_ptr;
+				new_ptr++;
+				*new_ptr = ' ';
+				old_ptr++;
+			}
+			new_ptr++;
+			old_ptr++;
+		}
+	}
+	*new_ptr = '\0';
+	free(old_buf);
+	return (new_buf);
+}
--- shell.h
+++ shell.h
@@ -1,280 +1,109 @@
-#ifndef __SHELL_H__
-#define __SHELL_H__
+#ifndef SHELL_H
+#define SHELL_H
 
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <limits.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <stdbool.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <limits.h>
+#include <signal.h>
 
+#define FALSE 0
+#define TRUE 1
+#define NFOT 2
+#define MATCH 3
+#define PREFIX 4
+#define EXIT_SHELL 5
+#define SKIP_FORK 6
+#define DO_EXECVE 7
 
-/*for read/write buffers */
-#define READ_BUF_SIZE 2048
-#define WRITE_BUF_SIZE 2048
-#define BUF_FLUSH -1
-
-/* for command chaining */
-#define CMD_NORM	0
-#define CMD_OR		1
-#define CMD_AND		2
-#define CMD_CHAIN	3
-
-/* for convert_number() */
-#define CONVERT_LOWERCASE	1
-#define CONVERT_UNSIGNED	2
-
-/* 1 if using system getline() */
-#define USE_GETLINE 0
-#define USE_STRTOK 0
-
-#define HIST_FILE ".my_simple_shell-history"
-#define HIST_MAX   4096\
+/**
+ * struct Alias - singly linked list
+ * @name: name of alias
+ * @value: command that alias calls
+ * @next: points to next node
+ */
+typedef struct Alias_lst
+{
+	char *key;
+	char *value;
+	struct Alias_lst *next;
+} alias;
 
 extern char **environ;
 
-/**
- * struct liststr -singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
-typedef struct liststr
-{
-	int num;
-	char *str;
-	struct liststr *next;
-}
-list_t;
+extern int status;
 
-/**
- * struct passinfo - contains pseudo-arguments to pass into a function.
- * @arg: a string generated from getline containing arguments
- * @agrv: an array of strings generated from arg
- * @args: a character array
- * @path: a string path for the current command
- * @argc: The argument count
- * @line_count: the error count
- * @err_num: the error code for exit()s
- * @line_count_flag: if on count this line of input
- * @fname: the program filename
- * @env: linked list local copy of environ
- * @environ: custom modified copy of environ
- * @history: the history node
- * @env_changed: on if environ was changed
- * @status: the return status of the last executed command
- * @cmd_buf: address of pointer to cmd_buf, on if chaining
- * @cmd_buf_type: CMD_type ||, &&,;
- * @readfd: the fd from which to read line input
- * @histcount: the history line number count
- */
-typedef struct passinfo
-{
-	char *arg;
-	char **argv;
-	char *path;
-	int argc;
-	int builtin_table;
-	unsigned int line_count;
-	int err_num;
-	int line_count_flag;
-	char *fname;
-	list_t *env;
-	list_t *history;
-	list_t *alias;
-	char **environ;
-	int env_changed;
-	int status;
+extern int line_num;
 
-	char **cmd_buf;/*pointer to cmd: chain buffer for memmory management*/
-	int cmd_buf_type; /* CMD_type ||, &&, */
-	int readfd;
-	int histcount;
-} info_t;
+extern char *shell_name;
 
-#define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-	0, 0, 0}
+int process_command(char **args);
 
-/**
- * struct builtin - contains a builtin string and related function
- * @type: the builtin command flag
- * @func: the function
- */
-typedef struct builtin
-{
-	char *type;
-	int (*func)(info_t *);
-} builtin_table;
+int built_ins(char **args);
 
-#define MAX_LENGTH 100 /* Or any other suitable value */
+int and_or(char **args, char operator, int last_compare);
 
-/* Caro&Josh_shloop.c */
-/*These function declaration are part of the shloop.c file*/
- /* they are related to the main loop and execution of commands in the shell */
-int hsh(info_t *, char **);
-int find_builtin(info_t *);
-void find_cmd(info_t *);
-void fork_cmd(info_t *);
+char *check_command(char **args);
 
-/* Caro&Josh_parser.c */
-/* These functions are relating to the parsing and handling commands */
- /* within the shell */
-int is_cmd(info_t *, char *);
-char *dup_chars(char *, int, int);
-char *find_path(info_t *, char *, char *);
+int execute_command(char **args);
 
-/* Caro&Josh_loophsh.c */
-/* this function is likely the entry point or the main function for shell */
-int loophsh(char **);
+char *sanitize_input(char *old_buf, size_t *old_size);
 
-/* Caro&Josh_errors.c */
-/*They likely provide error handling and printing functions. */
-void _eputs(char *);
-int _eputchar(char);
-int _putfd(char c, int fd);
-int _putsfd(char *str, int fd);
+int check_input_error(char *ptr);
 
-/* Caro&Josh_string.c */
-/*They likely provide various string manipulation functions */
-int _strlen(char *);
-int _strcmp(const char *, const char *);
-char *starts_with(const char *, const char *);
-char *_strcat(char *, char *);
+void print_error_msg(char *arg0, char *arg1);
 
-/* Caro&Josh_string1.c */
-/* they likely provide additional string manipulation functions.*/
-char *_strcpy(char *, char *);
-char *_strdup(const char *);
-void _puts(char *);
-int _putchar(char);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-/* Caro&josh_exits.c */
-/* they provide functions related th string operations and manipulation,*/
-char *_strncpy(char *, char *, int);
-char *_strncat(char *, char *, int);
-char *_strchr(char *, char);
+int _getline(char **line_ptr, size_t *n, int file);
 
-/* Caro&Josh_tokenizer.c */
-/* this is relating to the string tokenization */
-char **strtow(char *, char *);
-char **strtow2(char *, char);
+char *check_var(char *arg);
 
-/* Caro&Josh_realloc.c*/
-/* this is for memory management and reallocation */
-char *_memset(char *, char, unsigned int);
-void ffree(char **);
-void *_realloc(void *, unsigned int, unsigned int);
+int _strlen(char *str);
 
-/* Caro&Josh_memory.c*/
-/* This for memory deallocation */
-int bfree(void **);
+char *_strdup(char *src);
 
-/* Caro&Josh_atoi.c */
- /* this is for string manipulations and conversion.*/
-int interactive(info_t *);
-int is_delim(char, char *);
-int _isaplha(int);
-int _atoi(char *);
+char *str_concat(char *s1, char *s2);
 
-/* Caro&Josha_errors1.c */
-/* this is also for error handling and string manipulation.*/
-int _erratoi(char *);
-void print_error(info_t *, char *);
-int print_d(int, int);
-char *convert_number(long int, int, int);
-void remove_comments(char *);
+int str_comp(char *s1, char *s2, int pref_or_match);
 
-/* Caro&Josh_builtin.c */
-/* this is for the built-in shell commands.*/
-int _my_exit(info_t *);
-int _mycd(info_t *);
-int _myhelp(info_t *);
+char *get_array_element(char **array, char *element_name);
 
-/* Caro&Josh_builtin1.c */
- /* these are additional functions for the built-in shell commands*/
-int _myhistory(info_t *);
-int _myalias(info_t *);
-int unset_alias(info_t *, char *);
-int set_alias(info_t *, char *);
-int print_alias(list_t *);
-int find_builtin(info_t *);
+char **create_array(char *str, char delim, char **if_sep);
 
-/* Caro&Josh_getline.c */
- /* this handles reading user input from the command line*/
-ssize_t get_input(info_t *);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+int _listlen(char **list, char *entry);
 
-/* Caro&Josh_getinfo.c */
- /* this involves managing and manipulating the 'info_t' structure */
-  /* which stores information about the shell.*/
-void init_info(info_t *);
-void set_info(info_t *, char **);
-void free_info(info_t *, int);
+char **copy_array(char **old_array, int new_size);
 
-/* Caro&Josh_environ.c */
- /* It handles environment related operations and built-in commands */
- /* related to environment variables*/
-char *_getenv(info_t *info, const char *);
-int _my_env(info_t *);
-int _my_setenv(info_t *);
-int _my_unsetenv(info_t *);
-int populate_env_list(info_t *);
+int free_array(char **args);
 
-/* Caro&Josh_getenv.c */
- /* this provides additional funcions for managing environment variables*/
-char **get_environ(info_t *);
-int _unset_env_var(info_t *, char *);
-int _set_env_var(info_t *, char *, char *);
+int _setenv(const char *name, const char *value);
 
-/* Caro&Josh_history.c */
- /* this handles the history feature of the shell */
- /* and allows storing and retrieving previously executed commands*/
-char *get_history_fil(info_t *info);
-int write_history(info_t *info);
-int read_history(info_t *info);
-int build_history_list(info_t *info, char *buf, int line_count);
-int renumber_history(info_t *info);
+int _unsetenv(const char *name);
 
-/* Caro&Josh_lists.c */
- /* This provides function for manipulating linked lists.*/
-list_t *add_node(list_t **, const char *, int);
-list_t *add_node_end(list_t **, const char *, int);
-size_t print_list_str(const list_t *);
-int delete_node_at_index(list_t **, unsigned int);
-void free_list(list_t **);
+int _chngdir(char *name);
 
-/* Caro&Josh_lists1.c */
- /* this provides additional functions for manipulating linked lists*/
-size_t List_len(const list_t *);
-char **list_to_strings(list_t *);
-size_t print_list(const list_t *);
-list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
+int alias_handler(char **args, int free);
 
-/* Caro&Josh_vars.c */
- /* This is for handling variable substitution and string replacement.*/
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
-int replace_string(char **, char *);
+int free_alias(alias *_alias);
 
+int check_alias(char **args, alias *_alias);
 
-void initialize_info(info_t *info, int argc, char *argv[]);
-void handle_file_opening(info_t *info);
-void cleanup(info_t *info, int fd);
-void run_shell(info_t *info, char *argv[]);
-void print_error_message(char *program_name, char *file_name);
-void exit_with_error(int exit_code);
-int delete_node(list_t **head, list_t *node);
+int print_alias(alias *_alias);
 
+int get_alias(char *arg, alias *_alias);
 
+int set_alias(char *arg, alias *_alias, char *new_value);
+
+int print_env(void);
+
+char *_itos(int n);
+
+int _stoi(char *s);
 
 #endif
